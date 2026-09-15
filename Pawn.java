ebe3f7457353d423f33c284cee2b3a73d209e4fb
irenov05 <irenov05@gmail.com>
2026-09-14 22:37:37 -0600

    Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -11,4 +11,44 @@ public class Pawn {
         this.col = col;
         this.row = row;
     }
+    public String getPieceName() {
+        return pieceName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public char getColumn() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setColumn(char col) {
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public boolean verifyTarget(char newCol, int newRow) {
+
+        if (color.equals("WHITE")) {
+            if (col == newCol && row + 1 == newRow) {
+                return true;
+            }
+        }
+
+        if (color.equals("BLACK")) {
+            if (col == newCol && row - 1 == newRow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
